d7a7ee7f6db515c03ac9139c0d5a3df1f35b3a05
nghiatrngo <ngotrnghia1811@gmail.com>
2023-03-14 21:30:00 -0700

    Add mlfd package skeleton

---
+++ mlfd/__init__.py
@@ -0,0 +1,9 @@
+"""
+Multi-lingual Federated Distillation (MlFD)
+
+A framework for efficient federated learning over multilingual NLP models,
+combining semi-asynchronous tier-based training, personalized tokenizers,
+and federated knowledge distillation.
+"""
+
+__version__ = "1.0.0"
